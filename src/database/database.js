22eba6edb6d02ee843e458f27a7d0d970230cb2d
vizmarvizcaino <gatome2000@hotmail.com>
2022-10-29 18:08:14 -0500

    test_ci2

--- src/database/database.js
+++ src/database/database.js
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
     define: {
       timestamps: false
     }
