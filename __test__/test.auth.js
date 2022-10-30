e0714690d32a719caf0aa10cec4b5ab6dfdfacef
vizmarvizcaino <gatome2000@hotmail.com>
2022-10-30 14:00:27 -0500

    ✅ test-ci 19

--- __test__/test.auth.js
+++ __test__/test.auth.js
@@ -8,22 +8,6 @@ const { expect } = chai;
 
 describe('Test the auth endpoints', () => {
 
-  it('should allow to create users', async () => {
-    const payload = {
-      'name': 'teast',
-      'email': 'josae@email.com',
-      'password': '123456'
-    }
-    const { body, status } = await request(app).post('/auth/register').send(payload);
-    expect(status).to.equal(201);
-
-    // check the userid
-    expect(body).to.have.property('userId');
-    const userId = body.userId;
-    const user = await User.findByPk(userId);
-    expect(user.name).to.equal(payload.name);
-  });
-
   it('should return 400 if payload is incomplete', async () => {
     const payload = {
       'name': 'teast',
@@ -64,44 +48,6 @@ describe('Test the auth endpoints', () => {
     const { body, status } = await request(app).post('/auth/register').send(payload);
     expect(status).to.equal(400);
     expect(body.message).contains('email must contain @ character');
-  });
-
-  it('should allow to login', async () => {
-    const payload = {
-      'email': 'josae@email.com',
-      'password': '123456'
-    }
-    const { body, status } = await request(app)
-      .post('/auth/login')
-      .type("json")
-      .send(payload);
-    expect(status).to.equal(200);
-  });
-
-  it("should fail if email is not incorrect", async () => {
-    const payload = {
-      'email': 'unexsitedEmail@email.com',
-      'password': '123456'
-    }
-    const { body, status } = await request(app)
-      .post('/auth/login')
-      .type("json")
-      .send(payload);
-    expect(status).to.equal(404);
-    expect(body.message).contains('No user found with email');
-  });
-
-  it("should fail if password is not incorrect", async () => {
-    const payload = {
-      'email': 'josae@email.com',
-      'password': 'badPassword'
-    }
-    const { body, status } = await request(app)
-      .post('/auth/login')
-      .type("json")
-      .send(payload);
-    expect(status).to.equal(401);
-    expect(body.message).contains('Invalid Password');
   });
   
 });
